442916c309d770fac76d1c8023811aad24796832
Ediberto <edibertosilva33@gmail.com>
2024-10-13 18:25:45 -0300

    Delete Aula06 directory

--- Aula06/Excecao/src/excecao/OperacaoMatematica.java
+++
@@ -1,5 +0,0 @@
-package excecao;
-public abstract class OperacaoMatematica {
-    public abstract int calcular(int x, int y);
-}
-
--- Aula06/Excecao/src/excecao/Soma.java
+++
@@ -1,9 +0,0 @@
-package excecao;
-
-public class Soma {
-    public int calcular(int x, int y) {
-        int soma;
-        soma = x + y;
-        return soma;
-    }
-}
